1c24d500f0db3757aaa42ad742fe68bbed6fdb6d
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-24 01:49:27 +0200

    Promot pos from user input

--- Game.rb
+++ Game.rb
@@ -20,10 +20,24 @@ class Game
     end
 
     def valid_pos?(pos)
-        # ...
+        size = @board.size
+        return false if not pos.all? { |el| el < size && el >= 0 }
+        card = @board[pos]
+        return !card.revealed?
+    end
+
+    def make_pos(pos)
+        pos = pos[0].to_i, pos[1].to_i
+        pos
     end
 
     def make_guess
-
+        puts
+        print "Enter a position of card you want to flip ex.`1 2`: "
+        guessed_pos = gets.chomp.split
+        raise "Invalid position, should be like `row col`" if guessed_pos.length < 2
+        guessed_pos = make_pos(guessed_pos)
+        raise "Invalid position" if !valid_pos?(guessed_pos)
+        guessed_pos
     end
 end
