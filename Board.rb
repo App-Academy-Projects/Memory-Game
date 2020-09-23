efb2324ba486760a551ef009f04ebc6d15d69824
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-23 15:11:34 +0200

    Initialize methods for Board class

--- Board.rb
+++ Board.rb
@@ -1,5 +1,31 @@
 class Board
-    def initialize
+    attr_accessor :grid
+    def initialize(size)
+        @size = size
+        @grid = Array.new(size) { [] }
+    end
+
+    def populate
+        
+    end
+
+    def render
+        
+    end
+
+    def won?
+        
+    end
+
+    def reveal
+        
+    end
+
+    def [](pos)
+        
+    end
+
+    def []=(pos, value)
         
     end
 end
