d6d9eb2600832953db6c514e0958ea3a502c941c
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-23 14:35:08 +0200

    Initialize required methods for Card class

--- Card.rb
+++ Card.rb
@@ -1,5 +1,25 @@
 class Card
     def initialize
+        @face_up = false
+    end
+
+    def hide
+        self.face_up = false
+    end
+
+    def reveal
+        self.face_up = true
+    end
+
+    def to_s
+        
+    end
+
+    def ==
+        
+    end
+
+    def display_card_info
         
     end
 end
