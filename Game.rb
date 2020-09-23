e3c898822541cb197ec6dcdcf9b20da7136b8a7a
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-24 01:48:49 +0200

    Initialize the Game class

--- Game.rb
+++ Game.rb
@@ -1,5 +1,29 @@
+require_relative 'Board'
+
 class Game
-    def initialize
-        
+    def initialize(size)
+        @board = Board.new(size)
+        @previous_guess = nil
+
+        @board.populate
+    end
+
+    def play
+        while !over?
+            # ...
+        end
+        puts "You Won!!!"
+    end
+
+    def over?
+        @board.won?
+    end
+
+    def valid_pos?(pos)
+        # ...
+    end
+
+    def make_guess
+
     end
 end
